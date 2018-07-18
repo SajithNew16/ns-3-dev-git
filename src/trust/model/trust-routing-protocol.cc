eca642b88776604fdbde479ad852176fbaf6830a
sajithnew16@gmail.com <hypertonic16>
2018-07-18 14:35:46 +0530

    refactored route input

--- src/trust/model/trust-routing-protocol.cc
+++ src/trust/model/trust-routing-protocol.cc
@@ -665,14 +665,7 @@ RoutingProtocol::RouteInput (Ptr<const Packet> p, const Ipv4Header &header,
           if (it->getDestinationNode () == origin)
             {
               it->incNDR ();
-              if (origin == "10.0.0.3")
-                {
-                  for (int i = 0; i < 20; i++)
-                    {
-                      it->incNDR ();
-                    }
-                }
-              if (origin == "10.0.0.5")
+              if (origin == "10.0.0.3" || origin == "10.0.0.5")
                 {
                   for (int i = 0; i < 20; i++)
                     {
